43ce22444536f346c9c003a35a7318daf0ffaad8
Miguel Velez <miguelvelez@mijecu25.com>
2021-02-25 14:08:10 -0500

    obtained the path of the active workspace

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {Uri} from 'vscode';
+import * as path from 'path';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "perf-debug" is now active!');
@@ -13,29 +14,56 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let globalModel = vscode.commands.registerCommand('globalModel.start', () => {
-        // Create and show a new webview
-        const panel = vscode.window.createWebviewPanel(
-            'globalModel', // Identifies the type of the webview. Used internally
-            'Global Model', // Title of the panel displayed to the user
-            vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-            {} // Webview options. More on these later.
-        );
-    });
-    context.subscriptions.push(globalModel);
-
-    let localModels = vscode.commands.registerCommand('localModels.start', () => {
-        // Create and show a new webview
-        const panel = vscode.window.createWebviewPanel(
-            'localModels', // Identifies the type of the webview. Used internally
-            'Local Models', // Title of the panel displayed to the user
-            vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-            {} // Webview options. More on these later.
-        );
-    });
-    context.subscriptions.push(localModels);
+    let globalModel = vscode.commands.registerCommand('globalModel.start', _globalModel);
+    let localModels = vscode.commands.registerCommand('localModels.start', _localModels);
+    context.subscriptions.push(globalModel, localModels);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+    console.log('Deactivating extension "perf-debug"');
+}
+
+function _localModels() {
+    // Create and show a new webview
+    const panel = vscode.window.createWebviewPanel(
+        'localModels', // Identifies the type of the webview. Used internally
+        'Local Models', // Title of the panel displayed to the user
+        vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+        {} // Webview options. More on these later.
+    );
+}
+
+function _globalModel() {
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (!workspaceFolders) {
+        deactivate();
+        return;
+    }
+
+    // Create and show a new webview
+    const panel = vscode.window.createWebviewPanel(
+        'globalModel', // Identifies the type of the webview. Used internally
+        'Global Model', // Title of the panel displayed to the user
+        vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+        {} // Webview options. More on these later.
+    );
+
+    const onDiskPath = vscode.Uri.file(path.join(workspaceFolders[0].uri.path, 'cat.gif'));
+    const catGifSrc = panel.webview.asWebviewUri(onDiskPath);
+    panel.webview.html = getContent(catGifSrc);
+}
+
+function getContent(cat: Uri) {
+    return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <img src="${cat}" width="300" />
+</body>
+</html>`;
 }
