feed63c3a2df0c6f5e7bc2a8d0f26aaf64de6e42
Miguel Velez <miguelvelez@mijecu25.com>
2021-04-08 11:33:58 -0400

    reduced height of tables

--- src/extension.ts
+++ src/extension.ts
@@ -1119,7 +1119,7 @@ function getHotspotDiffContent(rawConfigs: string[], names2ConfigsRaw: any, meth
                                 
                 const influencingOptionsTable = new Tabulator("#influencingOptions", {
                     layout: "fitColumns",
-                    maxHeight:"450px",
+                    maxHeight:"400px",
                     // groupBy:"change",
                     groupStartOpen:function(value){
                         return value === 'Actual Influencing Options Changes';
@@ -1185,7 +1185,7 @@ function getHotspotDiffContent(rawConfigs: string[], names2ConfigsRaw: any, meth
                 const table = new Tabulator("#hotspot-diff-table", {
                     layout:"fitData",
                     // layout:"fitColumns",
-                    maxHeight:"450px",
+                    maxHeight:"400px",
                     dataTree:true,
                     dataTreeStartExpanded:false,
                     movableColumns: true, 
@@ -1712,7 +1712,7 @@ function getGlobalModelContent(names2PerfModelsRaw: any, rawConfigs: string[], n
                 
                 const localInfluenceTable = new Tabulator("#localInfluence", {
                     layout: "fitColumns",
-                    maxHeight:"450px",
+                    maxHeight:"400px",
                     columns: [
                         { title: "Influenced Hot Spot", field: "methods", sorter: "string" }, 
                         { title: "Influence (s)",  field: "influence",  sorter: influenceSort, hozAlign:"right" },
@@ -1750,7 +1750,7 @@ function getGlobalModelContent(names2PerfModelsRaw: any, rawConfigs: string[], n
                                       
                 const perfModelTable = new Tabulator("#perfModel", {
                     layout: "fitColumns",
-                    maxHeight:"450px",
+                    maxHeight:"400px",
                     selectable: true,
                     // groupBy:"change",
                     groupStartOpen:function(value){
